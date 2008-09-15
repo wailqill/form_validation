61d3d916271211253ba79e2ddb7a7bf54b311154
Glenn Nilsson <glenn.nilsson@gmail.com>
2008-09-15 22:53:04 +0200

    Added validator for length/range and min/max-length.

--- src/wq.form_validator.js
+++ src/wq.form_validator.js
@@ -68,6 +68,15 @@
     regexp: function(input, option) {
       return option.test(input);
     },
+    length: function(input, range) {
+      return range.include(input.length);
+    },
+    min_length: function(input, option) {
+      return input.length >= option;
+    },
+    max_length: function(input, option) {
+      return input.length <= option;
+    },
     EmailFormats: {
       // RegExp to follow the exact RFC defenition
       RFC2822: /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/,
