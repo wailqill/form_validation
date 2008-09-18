ce4b68b3e9b0c1129eef710883facb9cca2687a2
Glenn Nilsson <glenn.nilsson@gmail.com>
2008-09-18 14:11:25 +0200

    Updated the demo to use the new filename and Field members from error-messages branch.

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,6 @@
 $(document).observe("dom:loaded", function() {
   var fv = new wq.Form.Validation("demo-form");
-  fv.elements.add("#name", { min_length: 5 });
-  fv.elements.add("#email", { required: true, email: true });
-  fv.elements.add("#age", { required: true });
+  fv.fields.add("#name", { min_length: 5 });
+  fv.fields.add("#email", { required: true, email: true });
+  fv.fields.add("#age", { required: true });
 });
