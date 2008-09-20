bcd24df88b8c24f71ddc61270c5c93d7f40b5a07
Glenn Nilsson <glenn.nilsson@gmail.com>
2008-09-20 19:31:14 +0200

    Prepare for more samples.

--- demo/demo.js
+++
@@ -1,7 +0,0 @@
-$(document).observe("dom:loaded", function() {
-  var fv = new wq.Form.Validation("demo-form", {
-    "#name": { min_length: 5 },
-    "#email": { required: true, email: true },
-    "#age": { required: true, integer: "positive" }
-  });
-});
